8142d1ee5f80fed7d06527e7a7d584fa5f12ba27
ChrisNguyen99 <christopher.p.nguyen-1@ou.edu>
2021-03-15 12:39:51 +0000

    finished main

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # cs5293sp21-project0
 project 0 of Text Analytics
+Christopher Nguyen
+getting list of natures from incident pdf on norman pd site with count of each incident
+python3 on linux
+pipenv, PyPDF2, sqlite3, re, urllib should be installed
+
+Instructions:
+get code using git clone into desired directory git@github.com:ChrisNguyen99/cs5293sp21-project0.git
+ls into project0
+run using: pipenv run python project0/main.py --incidents <url>
+replace <url> with pdf file site so for example: pipenv run python project0/main.py --incidents https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf
+
+Functions:
+fetchincidents(url): uses urllib request to open website passed in and store in a temporary file
+extractincidents(incident_data): uses temp file to read pdf data and extract text using PyPDF2 file reader, each page is read and stored in a 
+
+Bugs/Assumptions:
+gps coordinates are not read into location
+the number of entries in the table is less than the total records in the pdf, regex didn't catch everything
+
+
--- project0/main.py
+++ project0/main.py
@@ -1,19 +1,25 @@
 import argparse
 import urllib
-import project0
-import PyPDF2
+import argparse
+from PyPDF2 import PdfFileReader, PdfFileWriter
 import tempfile
 import re
+from urllib import request
+import sqlite3
 
 def fetchincidents(url):
-    url = ("https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf")
+   # url = ("https://www.normanok.gov/sites/default/files/documents/2021-03/2021-03-01_daily_incident_summary.pdf")
     
     headers = {}
     headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
     
     data = urllib.request.urlopen(urllib.request.Request(url, headers=headers)).read()
+    file = open('temp.txt', 'wb')
+    file.write(data)
+    file.close()
 
 def extractincidents(data):
+    data = open('temp.txt', 'rb')
     fp = tempfile.TemporaryFile()
     row = []
     arr = []
@@ -24,72 +30,104 @@ def extractincidents(data):
     fp.seek(0)
     
     #read pdf
-    pdfReader = PyPDF2.pdf.PdfFileReader(fp) 
+    pdfReader = PdfFileReader(fp) 
     
     #loop through pages and extract to array
     for x in range(pdfReader.getNumPages()):
-        # Get first page
-        page1 = pdfReader.getPage(0).extractText()
-        row = page1.split("\n")
-        for y in row
+        # Get page
+        page = pdfReader.getPage(x).extractText()
+        row = page.split("\n")
+        for y in row:
             arr.append(y)
-    
+   
     return arr
     
 def createdb():
     con = sqlite3.connect('normanpd.db')
     cur = con.cursor()
-    
-    cur.execute("CREATE TABLE incidents(
-        incident_time TEXT,
-        incident_number TEXT,
-        incident_location TEXT,
-        nature TEXT,
+    cur.execute("DROP TABLE IF EXISTS incidents")
+    cur.execute("CREATE TABLE IF NOT EXISTS  incidents(\
+        incident_time TEXT,\
+        incident_number TEXT,\
+        incident_location TEXT,\
+        nature TEXT,\
         incident_ori TEXT)")
     con.commit()
     
-def populatedb(db, incidents)
+def populatedb(db, incidents):
     con = sqlite3.connect('normanpd.db')
     cur = con.cursor()
-    incidents.pop(0)
-    incidents.pop(0)
-    incidents.pop(0)
-    for row in incidents:
-        time = re.search(r"3\w*2021 \w+:\w+\b", row)
-        num = re.search(r"2021-\w+\b", row)
-        loc = re.search(r"\b[A-Z0-9] \t+ ^[A-Z]$", row)
-        nature = re.search(r"\w[A-Z][a-z]* (\s[A-Z][a-z]*)*)) ", row)
-        ori = re.search("\w[A-Z]+", row)
-        temp = [time,num,loc,nature,ori]
-        cur.execute("INSERT INTO incidents VALUES(?,?,?,?,?)", temp)
+        
+    time = []
+    num = []
+    loc = []
+    nat = []
+    ori = []
+    temp = []
+    #remove headers
+    incidents.remove('NORMAN POLICE DEPARTMENT')
+    incidents.remove('Daily Incident Summary (Public)')
+    incidents.remove('Date / Time')
+    incidents.remove('Incident Number')
+    incidents.remove('Location')
+    incidents.remove('Nature')
+    incidents.remove('Incident ORI')
+    for str in incidents:
+        match = re.search('^3/', str)
+        if match != None:
+            time.append(str)
+        match = re.search('^2021-', str)
+        if match != None:
+            num.append(str)
+        match = re.search('[A-Z0-9\s ]+ [A-Z0-9\s ]+ [A-Z0-9 ]+', str)
+        if match != None:
+            loc.append(str)
+        match = re.search('[A-Z]+[a-z]+', str)
+        if match != None:
+            nat.append(str)
+        match = re.search('^[A-Z0-9]+$', str)
+        if match != None:
+            ori.append(str)
+    #extra header at end    
+    time.pop()
+
+    for x in range(len(loc)):
+        temp.append((time[x], num[x], loc[x], nat[x], ori[x]))
+    
+    cur.executemany("INSERT INTO incidents VALUES (?,?,?,?,?)", temp)
     con.commit()
     cur.close()
     con.close()
     
 def status():
     con = sqlite3.connect('normanpd.db')
-    cur - con.cursor()
-    cur.execute("SELECT nature FROM incidents ORDER BY nature")
+    cur = con.cursor()
+    natures = []
+    con.commit()
+    for row in cur.execute('SELECT nature, COUNT(nature) FROM incidents GROUP BY nature HAVING COUNT(nature) > 0'):
+        natures.append(row)
+    for nat in range(len(natures)):
+        print(natures[nat][0],'|',natures[nat][1],sep='')
     
 def main(url):
     #download data
-    incident_data = project0.fetchincidents(url)
+    incident_data = fetchincidents(url)
     
     #extract data
-    incidents = project0.extractincidents(incident_data)
+    incidents = extractincidents(incident_data)
     
     #create new database
-    db = project0.createdb()
+    db = createdb()
     
     #insert data
-    project0.populatedb(db, incidents)
+    populatedb(db, incidents)
     
     #print incident counts
-    project0.status(db)
+    status()
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parse.add_argument("--incidents", type=str, required=True, help="Incident summary url.")
+    parser.add_argument("--incidents", type=str, required=True, help="Incident summary url.")
     
     args = parser.parse_args()
     if args.incidents:
